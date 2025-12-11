3a41ff0775d27a0f91362cf11c360a9c861f302e
iamsinchana <hssinchana04@gmail.com>
2025-12-11 20:46:00 +0530

    adding changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -62,6 +62,20 @@ const Navbar = () => {
           </div>
             : <button onClick={() => navigate('/login')} className='bg-blue-500 text-white px-5 py-2 rounded-full font-light hidden md:block cursor-pointer hover:bg-blue-600'>Create account</button>
         }
+        <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+        {/*--------------------Mobile Menu----------------*/}
+        <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-50 overflow-hidden bg-white transition-all`}>
+          <div className='flex items-center justify-between px-5 py-6'>
+            <img className='w-36' src={assets.logo} alt="" />
+            <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+          </div>
+          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+            <NavLink onClick={() => setShowMenu(false)} to='/'>{({ isActive }) => <p className={`px-4 py-2 rounded inline-block ${isActive ? 'bg-primary text-white' : ''}`}>HOME</p>}</NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to='/doctors'>{({ isActive }) => <p className={`px-4 py-2 rounded inline-block ${isActive ? 'bg-primary text-white' : ''}`}>ALL DOCTORS</p>}</NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to='/about'>{({ isActive }) => <p className={`px-4 py-2 rounded inline-block ${isActive ? 'bg-primary text-white' : ''}`}>ABOUT</p>}</NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to='/contact'>{({ isActive }) => <p className={`px-4 py-2 rounded inline-block ${isActive ? 'bg-primary text-white' : ''}`}>CONTACT</p>}</NavLink>
+          </ul>
+        </div>
 
       </div>
     </div>
